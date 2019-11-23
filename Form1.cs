345c66f1dedf1ee6e60f5a7335001a40adc0e28c
m-nasir928 <57988562+m-nasir928@users.noreply.github.com>
2019-11-23 19:06:24 +0500

    AOA File Discoverer Project

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace AOAprojectpart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\Mahnoor Nasir\Desktop\New folder";
+            string[] folders = Directory.GetDirectories(path);
+
+            foreach (string folder in folders)
+            {
+                string partialName = textBox1.Text;
+                DirectoryInfo hdDirectoryInWhichToSearch = new DirectoryInfo(folder);
+                FileInfo[] filesInDir = hdDirectoryInWhichToSearch.GetFiles("*" + partialName + "*.*");
+
+                foreach (FileInfo foundFile in filesInDir)
+                {
+                    string fullName = foundFile.FullName;
+                    listfiles.Items.Add(fullName);
+                }
+            }
+        }
+    }
+}
